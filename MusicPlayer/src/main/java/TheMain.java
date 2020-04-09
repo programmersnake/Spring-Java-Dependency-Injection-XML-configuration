7d48aeeb0b66ab52696a3c103b46b414f0a287f1
Denis <45206633+programmersnake@users.noreply.github.com>
2020-04-09 12:07:43 -0700

    Add files via upload

---
+++ MusicPlayer/src/main/java/TheMain.java
@@ -0,0 +1,14 @@
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import java.util.Scanner;
+
+public class TheMain {
+
+    public static void main(String[] args){
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.tld");
+        MusicPlayer player = context.getBean("MusicPlayerBean", MusicPlayer.class);
+        player.play();
+        context.close();
+    }
+
+}
